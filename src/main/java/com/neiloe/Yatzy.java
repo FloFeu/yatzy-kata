5624ae3f0df1e6aefac66e3b9427d1e5d498cd9d
Florent Feugere <florent.feugere@gmail.com>
2022-03-15 16:51:12 +0100

    Delete useless constructor

--- src/main/java/com/neiloe/Yatzy.java
+++ src/main/java/com/neiloe/Yatzy.java
@@ -65,17 +65,6 @@ public class Yatzy {
         return sum;
     }
 
-    protected int[] dice;
-
-    public Yatzy(int d1, int d2, int d3, int d4, int d5) {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = d5;
-    }
-
     public static int pair(int d1, int d2, int d3, int d4, int d5) {
         int[] counts = new int[6];
         counts[d1 - 1]++;
